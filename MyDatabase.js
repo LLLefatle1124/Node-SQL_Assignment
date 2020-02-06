dd2c4295ddcd62b2e54e41edc10b91095352759f
lllefatle1124 <lawrence.lefatle@gmail.com>
2020-02-06 16:58:27 +0200

    cant update records most done

--- MyDatabase.js
+++ MyDatabase.js
@@ -1,28 +1,91 @@
-
-// npm install --save pg
-// find out more here: https://node-postgres.com/
-
-const Pool = require("pg").Pool;
-const pool = new Pool({
+// establishing a connection to a database
+const {Client} = require("pg");
+const client = new Client({
   user: "user",
   host: "localhost",
-  database: "MyDatabase",
+  database: "mydatabase",
   password: "pass",
   port: 5432
 });
 
-const addNewVisitor = (name, age, visit_date, visit_time, assistant_name, comments) => {
-    pool.query(
-    `INSERT INTO visitors(visitor_name, visitor_age, date_of_visit, time_of_visit, name_of_assistant, comments)VALUES($1, $2, $3, $4, $5, $6)`,
-    [name, age, visit_date, visit_time, assistant_name, comments],
-        (error, results) => {
-            if(error){
-                throw error;
-            }
-            console.log(results.rows)
-        }
-        )
+client.connect();
+// function to save a new visitor to MyDatabase
+const addNewVisitor = async(name, age, visit_date, visit_time, assistant_name, comments) => {
+    try {
+        let results = await client.query(
+
+        `INSERT INTO visitors(
+            visitor_name, 
+            visitor_age, 
+            date_of_visit, 
+            time_of_visit, 
+            name_of_assistant, 
+            comments)
+            VALUES($1, $2, $3, $4, $5, $6)
+            RETURNING *;`,
+
+        [name, age, visit_date, visit_time, assistant_name, comments]
+
+        );
+        return results.rows;
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
 };
 
-addNewVisitor("posh", 12, "04/02/2020", "15:50", "Sihle", "non");
+// addNewVisitor("Nolo", 12, "04/02/2020", "15:50", "Sihle", "none");
+// addNewVisitor("KC", 12, "04/02/2020", "15:50", "Sihle", "none");
+// addNewVisitor("Sihle", 12, "04/02/2020", "15:50", "Sihle", "none");
+// addNewVisitor("Sbongile", 13, "04/02/2020", "15:50", "Sihle", "none");
+
+const listAllVisits = async() => {
+    try {
+        let SQL = `SELECT visitor_ID, visitor_name FROM visitors;`
+        let results = await client.query(SQL);
+        console.log("Return for listAllVisits: " + results.rows);
+        return results.rows;
+    } catch (e) {
+        throw e;
+    }
+};
+
+listAllVisits();
+
+// taking an ID parameter because I need to delete a specific record
+const deleteVisit = async(id) => {
+    try {
+        await client.query('DELETE FROM visitors WHERE visitor_ID=$1;', [id]);
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
+};
+
+deleteVisit(1);
+deleteVisit(3);
+
+// taking row, a condition and value parameters because I need to know where, when and what am i updating in a record
+const updateVisit = async(id, where, value) => {
+    try {
+        await client.query('UPDATE visitors SET $2 = $3 WHEN visitor_ID=$1;', [id, where, value]);
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
+};
+
+updateVisit(4, 'visitor_name', 'Shan');
+
+// const veiwVisit = () => {
+
+// };
+
+// veiwVisit()
+
+// const emptyVisits = () => {
+
+// };
+
+// emptyVisits()
 
--- Visiters.sql
+++ Visiters.sql
@@ -1,9 +1,10 @@
-DROP TABLE IF EXISTS "visitors";
-CREATE TABLE "visitors"(
-    "visitor_name" text NOT NULL,
-    "visitor_age" integer NOT NULL,
-    "date_of_visit" date NOT NULL,
-    "time_of_visit" time without time zone NOT NULL,
-    "name_of_assistant" text NOT NULL,
-    "comments" text NOT NULL
+DROP TABLE IF EXISTS visitors;
+CREATE TABLE visitors(
+    visitor_ID SERIAL PRIMARY KEY,
+    visitor_name varchar NOT NULL,
+    visitor_age integer NOT NULL,
+    date_of_visit date NOT NULL,
+    time_of_visit time NOT NULL,
+    name_of_assistant varchar NOT NULL,
+    comments varchar NOT NULL
 );
