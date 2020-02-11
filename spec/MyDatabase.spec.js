3a079162e47175c74b1b45a3d5c70aa7658912a4
lllefatle1124 <lawrence.lefatle@gmail.com>
2020-02-11 14:38:53 +0200

    all jasmine working

--- spec/MyDatabase.spec.js
+++ spec/MyDatabase.spec.js
@@ -1,49 +1,101 @@
-let {
+const {
     emptyVisits,
     veiwVisit,
     updateVisit,
     deleteVisit,
     listAllVisits,
     addNewVisitor
-} = require("../src/MyDatabase")
+} = require("../src/MyDatabase");
 
 let obj = {
+  date_of_visit: new Date("04/02/2020")
+}
 
-    visitor_name: "KC",
-    visitor_age: 12,
-    date_of_visit: new Date("04/02/2020"),
-    time_of_visit: "15:50:00",
-    name_of_assistant: "Sihle",
-    comments: "none"
-};
+describe("node sql addNewVisitor", function() {   
+    it("save data to the database", async function(done) {
 
-let obj1 = {
+        objDetails = await addNewVisitor("Nhlanhla Lefatle", 23, "04/02/2020", "11:00:00", "Wandile Nxumalo", "later darling");
+        addNewVisitor("Nolo", 12, "04/02/2020", "11:00:00", "Sihle", "none");
+        addNewVisitor("KC", 12, "04/02/2020", "11:00:00", "Sihle", "none");
+        addNewVisitor("Sihle", 12, "04/02/2020", "11:00:00", "Sihle", "none");
+        addNewVisitor("Sbongile", 13, "04/02/2020", "11:00:00", "Sihle", "none");
 
-    visitor_name: "KC"
+        expect(objDetails[0].visitor_name).toEqual("Nhlanhla Lefatle");
+        expect(objDetails[0].visitor_age).toEqual(23);
+        expect(objDetails[0].date_of_visit).toEqual(obj.date_of_visit);
+        expect(objDetails[0].time_of_visit).toEqual("11:00:00");
+        expect(objDetails[0].name_of_assistant).toEqual("Wandile Nxumalo");
+        expect(objDetails[0].comments).toEqual("later darling");
 
-};
-
-describe("AddNewVisitor function", () => {
-    it("Adds a new visiter record to a visitors table", async(done) => {
-        record = await addNewVisitor("KC", 12, "04/02/2020", "15:50:00", "Sihle", "none");
         done();
-        expect(record[0].visitor_name).toEqual(obj.visitor_name);
-        expect(record[0].visitor_age).toEqual(obj.visitor_age);
-        expect(record[0].date_of_visit).toEqual(obj.date_of_visit);
-        expect(record[0].time_of_visit).toEqual(obj.time_of_visit);
-        expect(record[0].name_of_assistant).toEqual(obj.name_of_assistant);
-        expect(record[0].comments).toEqual(obj.comments);
-    })
-})
 
-describe("listAllVisits function", () => {
-    it("list all visitors from a visitors table", async(done) => {
-        record = await listAllVisits();
+    }); 
+});
+
+describe("node sql listAllVisits", function() {   
+    it("should be able to list all visitors", async function(done){
+
+        let list = await listAllVisits();
+
+        expect(list[0].visitor_name).toEqual("Nhlanhla Lefatle");
+        expect(list[0].visitor_id).toBeDefined();
+        expect(list[1].visitor_name).toEqual("Nolo");
+        expect(list[1].visitor_id).toBeDefined();
+        expect(list[2].visitor_name).toEqual("KC");
+        expect(list[2].visitor_id).toBeDefined();
+        expect(list[3].visitor_name).toEqual("Sihle");
+        expect(list[3].visitor_id).toBeDefined();
+        expect(list[4].visitor_name).toEqual("Sbongile");
+        expect(list[4].visitor_id).toBeDefined();
+
         done();
-        delete record[0].visitor_id;
-        
-        expect(record[0]).toEqual(obj1);
-        // expect(record).toEqual(obj.name_of_assistant);
-        // expect(record).toEqual(obj.comments);
-    })
-})
+
+      });
+});
+
+describe("node sql deleteVisit,", function() { 
+    it("should be able to delete a visitor", async function(done){
+
+        let deleted = await deleteVisit(3);
+        expect(deleted.command).toBe('DELETE');
+        expect(deleted.rowCount).toBe(1);
+        done();
+
+    });
+});
+
+describe("node sql updateVisit,", function() { 
+    it("should be able to update a visitor's details", async function (done) {
+
+        let update = await updateVisit(2, "visitor_name", "LEBOHANG");
+
+        expect(update.command).toEqual('UPDATE');
+        expect(update.rowCount).toEqual(1);
+
+        done();
+
+    }); 
+});
+
+describe("node sql veiwVisit,", function() {  
+    it("should be able to view a visitor", async function (done) {
+        let view =  await veiwVisit(4);
+
+        expect(view[0].visitor_name).toBe("Sihle");
+        expect(view[0].visitor_age).toEqual(12);
+        expect(view[0].date_of_visit).toEqual(obj.date_of_visit);
+        expect(view[0].time_of_visit).toEqual("11:00:00");
+        expect(view[0].name_of_assistant).toEqual("Sihle");
+        expect(view[0].comments).toEqual("none");
+
+        done();
+    }); 
+});
+
+describe("node sql emptyVisits,", function() { 
+    it("should be able to delete all visitors", async function (done) {
+
+        expect(await emptyVisits()).not.toBeNull();
+        done();
+    });
+});
--- src/MyDatabase.js
+++ src/MyDatabase.js
@@ -8,15 +8,8 @@ const client = new Client({
   port: 5432
 });
 
-const myclient = new Client({
-    user: "user",
-    host: "localhost",
-    database: "MyDatabase",
-    password: "pass",
-    port: 5432
-  });
-
 client.connect();
+
 // function to save a new visitor to MyDatabase
 const addNewVisitor = async(name, age, visit_date, visit_time, assistant_name, comments) => {
     try {
@@ -33,82 +26,65 @@ const addNewVisitor = async(name, age, visit_date, visit_time, assistant_name, c
             RETURNING *;`,
 
         [name, age, visit_date, visit_time, assistant_name, comments]
-
+        
         );
+
         return result.rows;
+
     } catch (e) {
         console.log(e);
         throw e;
     }
 };
 
-// addNewVisitor("Nolo", 12, "04/02/2020", "15:50", "Sihle", "none");
-// addNewVisitor("KC", 12, "04/02/2020", "15:50", "Sihle", "none");
-// addNewVisitor("Sihle", 12, "04/02/2020", "15:50", "Sihle", "none");
-// addNewVisitor("Sbongile", 13, "04/02/2020", "15:50", "Sihle", "none");
-
 const listAllVisits = async() => {
     const SQL = `SELECT visitor_ID, visitor_name FROM visitors;`
     try {
         query = await client.query(SQL);
-        console.log(query.rows);
         return query.rows;
     } catch (e) {
         throw e;
     }
 };
 
-// listAllVisits();
-
 // taking an ID parameter because I need to delete a specific record
 const deleteVisit = async(id) => {
     try {
-        await client.query('DELETE FROM visitors WHERE visitor_ID=$1;', [id]);
+        let deleted = await client.query(`DELETE FROM visitors WHERE visitor_ID=$1;`, [id]);
+        return deleted;
     } catch (e) {
         console.log(e);
         throw e;
     }
 };
-
-// deleteVisit(1);
-// deleteVisit(3);
 
 // taking row, a condition and value parameters because I need to know where, when and what am i updating in a record
 const updateVisit = async(id, where, value) => {
     try {
-        await client.query(`UPDATE visitors SET ${where}= $2 WHERE visitor_ID=$1;`, [id, value]);
+        let updated = await client.query(`UPDATE visitors SET ${where}= $2 WHERE visitor_ID=$1;`, [id, value]);
+        return updated;
     } catch (e) {
         console.log(e);
         throw e;
     }
 };
-
-// updateVisit(4, "visitor_name", "Shanny");
-// updateVisit(4, 'visitor_age', '22');
 
 const veiwVisit = async(id) => {
     try {
         const query = await client.query(`SELECT * FROM visitors WHERE visitor_ID=$1;`, [id]);
-        console.log(query.rows)
-        client.end();
+        return query.rows;
     } catch (e) {
         console.log(e);
-        client.end();
         throw e;
     }
-    emptyVisits();
 };
 
-// veiwVisit(4);
-
 const emptyVisits = async() => {
-    myclient.connect();
     try {
-        await myclient.query('DELETE FROM visitors;');
-        myclient.end();
+        let emptied = await client.query('DELETE FROM visitors;');
+        return emptied;
     } catch (e) {
         console.log(e);
-        myclient.end();
         throw e;
     }
 };
